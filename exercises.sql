66e3a03c2ca04f25fe88a0756d6be1c3a5d749c4
DanielFred1 <dsena.frederic@gmail.com>
2021-07-13 19:48:05 -0300

    Trabalhando com CTE

--- exercises.sql
+++ exercises.sql
@@ -66,6 +66,34 @@ JOIN cliente_transacoes
 JOIN tipo_transacao
 	ON tipo_transacao.id = cliente_transacoes.tipo_transacao_id;
 
+
+-- Utilização de CTE (Common Table Expressions)
+-- Retorna todos os clientes e transações no sistema
+WITH clientes_e_transacoes AS (
+	SELECT	cliente.nome AS cliente_nome,
+			tipo_transacao.nome AS tipo_transacao_nome,
+			cliente_transacoes.valor AS tipo_transacao_valor
+	FROM cliente_transacoes
+	JOIN cliente ON cliente.numero = cliente_transacoes.cliente_numero
+	JOIN tipo_transacao ON tipo_transacao.id = cliente_transacoes.tipo_transacao_id
+)
+SELECT cliente_nome, tipo_transacao_nome, tipo_transacao_valor
+FROM clientes_e_transacoes;
+
+-- Retorna apenas clientes e transações de um banco específico
+WITH clientes_e_transacoes AS (
+	SELECT	cliente.nome AS cliente_nome,
+			tipo_transacao.nome AS tipo_transacao_nome,
+			cliente_transacoes.valor AS tipo_transacao_valor,
+			banco.nome AS banco_nome
+	FROM cliente_transacoes
+	JOIN cliente ON cliente.numero = cliente_transacoes.cliente_numero
+	JOIN tipo_transacao ON tipo_transacao.id = cliente_transacoes.tipo_transacao_id
+	JOIN banco ON banco.numero = cliente_transacoes.banco_numero AND banco.nome ILIKE '%Itaú%'
+)
+SELECT cliente_nome, tipo_transacao_nome, tipo_transacao_valor, banco_nome
+FROM clientes_e_transacoes;
+
 	
 
 
